d934ca50c461feeb4c32489560f0b89e93a84806
JustNotAFox <JustNotAFox@gmail.com>
2017-11-12 22:23:51 -0500

    added python script to generate csv of data

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 Updated Readme
 Scoring: (n-2)^2
 Ruling: if there is a gap between groups of colors; Move everything to the left and down
+
+Heuristics
+Point value of move
+Total point value of moves after move is done
+Colors left (a move that removes an entire color is *probably* the best move?)
+Maximum possible remaining point value (sum of cells^2 for all colors)
+Number of groupings remaining in post-move state (another "less is more")
+
+Solutions genes an array of values
+Heuristic is a sum of the above multiplied by those values
+Values can be negative (important for heuristics #3 and #5)
+Breeding should either average these values or choose some and randomize the others? Thoughts?
+Mutation randomly changes a value.
---
+++ test.py
@@ -0,0 +1,174 @@
+import random
+import datetime
+
+def genetic(g):
+    pop = []
+    for i in range(20):
+        heur = {}
+        heur["heur"] = []
+        for t in range(5):
+            heur["heur"].append(random.random())
+        pop.append(heur)
+    for i in pop:
+        i["fitness"] = solve(g,i["heur"])["score"]
+    for i in range(100):
+        z = max(pop,key=lambda t: t["fitness"])
+        x = random.randint(0,19)
+        while pop[x]["heur"] == z["heur"]:
+            x = random.randint(0, 19)
+        pop[x]["heur"] = mutate(pop[x]["heur"])
+        pop[x]["fitness"] = solve(g,pop[x]["heur"])["score"]
+        x = random.randint(0,19)
+        while pop[x]["heur"] == z["heur"]:
+            x = random.randint(0, 19)
+        y = random.randint(0,19)
+        pop[x]["heur"] = breed(pop[y]["heur"],z["heur"])
+        pop[x]["fitness"] = solve(g,pop[x]["heur"])["score"]
+
+    return pop
+
+def mutate(h):
+    h[random.randint(0,4)] = random.random()
+
+def breed(h,h2):
+    r = []
+    for i in range(5):
+        x = random.randint(1,4)
+        if x == 1:
+            r.append(h[i])
+            continue
+        if x == 2:
+            r.append(h2[i])
+            continue
+        if x == 3:
+            r.append((h[i] + h2[i]) / 2)
+            continue
+        if x == 4:
+            r.append(random.random())
+            continue
+    return r
+
+def woc(g,h):
+    score = 0
+    while listMoves(g):
+        m = []
+        for heur in h:
+            m.append(getMove(g,heur))
+        n = [i["spaces"][0] for i in m]
+        o = set(n)
+        while len(n) > len(o):
+            n = [i for i in n if not i in o or o.remove(i)]
+            o = set(n)
+        n = n[0]
+        m = [i for i in m if i["spaces"][0] == n][0]
+        score += m["score"]
+        g = m["after"]
+    return score
+
+
+def getMove(g, h):
+    m = listMoves(g)
+    im = []
+    for move in m:
+        a = {"spaces": move, "after": postMove(g.copy(), move)}
+        a["score"] = (len(a["spaces"]) - 2) ** 2
+        a["aftermoves"] = listMoves(a["after"])
+        total = 0
+        for group in a["aftermoves"]:
+            total += (len(group) - 2) ** 2
+        a["aftertotal"] = total
+        total = 0
+        for i in range(3):
+            total += (sum(1 for col in a["after"].values() if col == i + 1) - 2) ** 2
+        a["maxtotal"] = total
+        score = 0
+        score += h[0] * a["score"]
+        score += h[1] * len(a["aftermoves"])
+        score += h[2] * a["aftertotal"]
+        score += h[3] * a["maxtotal"]
+        if a["aftermoves"]:
+            score += h[4] * a["aftertotal"] / len(a["aftermoves"])
+        a["hscore"] = score
+        im.append(a)
+    return max(im, key=lambda t:t["hscore"])
+
+def checkMove(g, c, x, y):
+    m = [(x, y)]
+    if x > 0:
+        if (x - 1, y) not in c and g[x,y] == g[x-1,y]:
+            c.append((x - 1, y))
+            m.extend(checkMove(g,c,x - 1,y))
+    if y > 0:
+        if (x, y - 1) not in c and g[x,y] == g[x,y - 1]:
+            c.append((x, y - 1))
+            m.extend(checkMove(g,c,x,y - 1))
+    if x < 15:
+        if (x + 1, y) not in c and g[x,y] == g[x+1,y]:
+            c.append((x + 1, y))
+            m.extend(checkMove(g,c,x+1,y))
+    if y < 11:
+        if (x, y + 1) not in c and g[x,y] == g[x,y + 1]:
+            c.append((x, y + 1))
+            m.extend(checkMove(g,c,x,y + 1))
+    return m
+
+def postMove(g,m):
+    outx = 0
+    outy = 0
+    for x in range(16):
+        for y in range(12):
+            if (x,y) not in m:
+                g[(outx,outy)] = g[(x,y)]
+                outy += 1
+        if outy > 0:
+            for z in range(outy,12):
+                g[(outx,z)] = 0
+            outy = 0
+            outx += 1
+    for x in range(outx,16):
+        for y in range(12):
+            g[x,y] = 0
+    return g
+
+def listMoves(g):
+    checked = []
+    moves = []
+    for x in range(16):
+        if g[x,0] == 0:
+            break
+        for y in range(12):
+            if g[x, y] == 0:
+                break
+            if (x, y) not in checked:
+                checked.append((x,y))
+                moves.append(checkMove(g,checked,x,y))
+    return [move for move in moves if len(move) > 1]
+
+def genBoard(x,y):
+    g = {}
+    for x in range(16):
+        for y in range(12):
+            g[x,y] = random.randint(1,3)
+    return g
+
+def solve(g,h):
+    r = {}
+    r["score"] = 0
+    r["moves"] = []
+    while(listMoves(g)):
+        a = getMove(g,h)
+        r["score"] += a["score"]
+        r["moves"].append(a["spaces"])
+        g = a["after"]
+    return r
+
+f = open('out.csv', 'w')
+f.write("Run,Best,Worst,WOC,Time\n")
+c = 1
+while True:
+    time = datetime.datetime.now()
+    g = genBoard(1,1)
+    i = genetic(g)
+    i = sorted(i,key=lambda x:x["score"],reverse=True)
+    t = woc(g, [t["heur"] for t in i[:5]])
+    f.write(str(c) + ',' +  str(i[0]["score"]) + ',' + str(i[len(i) - 1]["score"]) + ',' + str(t,time-datetime.datetime.now()) + '\n')
